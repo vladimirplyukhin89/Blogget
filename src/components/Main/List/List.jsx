96fa46db45f597674c93d4c19275a05313c66374
Pluhin <Pluhin@itpc.ru>
2023-03-24 17:30:37 +0500

    Вывод ошибки bestPostsError

--- src/components/Main/List/List.jsx
+++ src/components/Main/List/List.jsx
@@ -1,4 +1,4 @@
-import { useContext } from "react";
+import { useEffect, useContext } from "react";
 
 import Post from "./Post";
 import s from "./List.module.css";
@@ -6,7 +6,13 @@ import s from "./List.module.css";
 import { postContext } from "../../../context";
 
 export const List = () => {
-  const { bestPosts } = useContext(postContext);
+  const { bestPosts, bestPostsError } = useContext(postContext);
+
+  useEffect(() => {
+    if (bestPostsError) {
+      return alert(`Сбой при получении постов: ${bestPostsError}`);
+    }
+  }, [bestPostsError]);
 
   return (
     <ul className={s.list}>
